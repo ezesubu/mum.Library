f1e064e56333cbe6a94ae60e89bd8794f3f88195
Ezequiel Suárez B <ezesubu@gmail.com>
2018-06-07 16:11:10 -0500

    data access v0.1

--- src/data/DataAccess.java
+++ src/data/DataAccess.java
@@ -8,14 +8,28 @@ import domain.Author;
 import domain.Book;
 
 public class DataAccess {
-	public void generateData() {
-	        Address address = new Address("1", "The otherSide", "No where", "Every Where", "0123456789");
-			Author author = new Author("1", "Arthur C", "Clarke", address, "was a British science fiction writer, science writer and futurist");
+			Address address; 
+			Author author;
 			List<Author> authors = new LinkedList<>();
-			authors.add(author);
 			List<Book> books = new LinkedList<>();
-			books.add(new Book("001", "2001: A Space Odyssey", 5, authors));
-			books.add(new Book("002", "3001: The Final Odyssey", 4, authors));
-			books.add(new Book("002", "3001: The Final Odyssey", 4, authors));
+			
+	public DataAccess() {
+		address = new Address("1", "The otherSide", "No where", "Every Where", "0123456789");
+		author= new Author("1", "Arthur C", "Clarke", address,
+				"was a British science fiction writer, science writer and futurist");
+		
+		authors.add(author);
+		
+		books.add(new Book("001", "2001: A Space Odyssey", 5, authors));
+		books.add(new Book("002", "3001: The Final Odyssey", 4, authors));
+		books.add(new Book("002", "3001: The Final Odyssey", 4, authors));
+	}
+	
+	public List<Book> getBooks(){
+		return books;
+	}
+	
+	List<Author> getAuthors(){
+		return authors;
 	}
 }
