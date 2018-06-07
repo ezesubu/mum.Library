7555b9428c6e33fda55bd7035bc1ade41d2a2019
bi.khatri@mum.edu <bi.khatri@mum.edu>
2018-06-07 17:02:54 -0500

    data access

--- src/domain/Book.java
+++ src/domain/Book.java
@@ -1,5 +1,6 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Book {
@@ -9,6 +10,41 @@ public class Book {
 	private List<Author> authors;
 	private List<BookCopy> bookCopies;
 	
+	public Book() {
+		authors = new ArrayList<>();
+		bookCopies = new ArrayList<>();
+	}
+	public String getISBN() {
+		return ISBN;
+	}
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Integer getMaxCheckoutDays() {
+		return maxCheckoutDays;
+	}
+	public void setMaxCheckoutDays(Integer maxCheckoutDays) {
+		this.maxCheckoutDays = maxCheckoutDays;
+	}
+	public List<Author> getAuthors() {
+		return authors;
+	}
+	public void setAuthors(List<Author> authors) {
+		this.authors = authors;
+	}
+	public List<BookCopy> getBookCopies() {
+		return bookCopies;
+	}
+	public void setBookCopies(List<BookCopy> bookCopies) {
+		this.bookCopies = bookCopies;
+	}
+
  	
 	
 }
--- src/domain/BookCopy.java
+++ src/domain/BookCopy.java
@@ -3,4 +3,24 @@ package domain;
 public class BookCopy {
 	private Book book;
 	private String bookCopyNum;
+	
+	public BookCopy() {
+		book = new Book();
+	}
+
+	public Book getBook() {
+		return book;
+	}
+
+	public void setBook(Book book) {
+		this.book = book;
+	}
+
+	public String getBookCopyNum() {
+		return bookCopyNum;
+	}
+
+	public void setBookCopyNum(String bookCopyNum) {
+		this.bookCopyNum = bookCopyNum;
+	}
 }
