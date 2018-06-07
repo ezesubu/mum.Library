9c49143181776d4b84827a0db8afba5f077c583e
Cong Khanh Tran <congkhanhtran@CS.MUM.EDU>
2018-06-07 15:36:17 -0500

    aaa

--- src/controller/CheckoutController.java
+++ src/controller/CheckoutController.java
@@ -1,9 +1,13 @@
 package controller;
-
+/**
+ * 
+ * @author Cong Khanh Tran (trancongkhanh@gmail.com)
+ * Checkout controller
+ */
 public class CheckoutController {
 
 	public void checkout() {
-//Khanh		
+		
 	}
 
 }
