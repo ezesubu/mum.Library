8b25618e06f0ba1ee025d9e10babc6f8a9743385
Ezequiel Suárez B <ezesubu@gmail.com>
2018-06-07 16:59:32 -0500

    finish add copy book

--- src/domain/BookCopy.java
+++ src/domain/BookCopy.java
@@ -3,4 +3,11 @@ package domain;
 public class BookCopy {
 	private Book book;
 	private String bookCopyNum;
+	
+	public BookCopy(Book book, String bookCopyNum) {
+		this.book = book;
+		this.bookCopyNum = bookCopyNum;
+	}
+	
+	
 }
