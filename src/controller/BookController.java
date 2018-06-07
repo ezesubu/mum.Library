e23bde64008c2e3d2dae0eeff7dd89d12787afc6
Ezequiel Suárez B <ezesubu@gmail.com>
2018-06-07 16:07:19 -0500

    data 1 version

--- src/controller/BookController.java
+++ src/controller/BookController.java
@@ -4,14 +4,21 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
+import domain.Address;
+import domain.Author;
 import domain.Book;
 
 public class BookController {
 
 	public void addBookCopy() {
+		//Arthur C. Clarke
+		Address address = new Address("1", "The otherSide", "No where", "Every Where", "0123456789");
+		Author author = new Author("1", "Arthur C", "Clarke", address, "was a British science fiction writer, science writer and futurist");
+		List<Author> authors = new LinkedList<>();
+		authors.add(author);
 		List<Book> books = new LinkedList<>();
-		books.add(new Book());
-//		Ezequel
+		books.add(new Book("001", "2001: A Space Odyssey", 5, authors));
+		books.add(new Book("002", "3001: The Final Odyssey",  5, authors));
 		//String n = System.getProperty("line.separator");
 		String choices = "Enter ISBN number:";
 		Scanner sc = new Scanner(System.in);
--- src/data/DataAccess.java
+++ src/data/DataAccess.java
@@ -1,7 +1,21 @@
 package data;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import domain.Address;
+import domain.Author;
+import domain.Book;
+
 public class DataAccess {
 	public void generateData() {
-		
+	        Address address = new Address("1", "The otherSide", "No where", "Every Where", "0123456789");
+			Author author = new Author("1", "Arthur C", "Clarke", address, "was a British science fiction writer, science writer and futurist");
+			List<Author> authors = new LinkedList<>();
+			authors.add(author);
+			List<Book> books = new LinkedList<>();
+			books.add(new Book("001", "2001: A Space Odyssey", 5, authors));
+			books.add(new Book("002", "3001: The Final Odyssey", 4, authors));
+			books.add(new Book("002", "3001: The Final Odyssey", 4, authors));
 	}
 }
